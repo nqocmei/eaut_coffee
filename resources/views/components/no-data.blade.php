a64535909fd71fe2e8595f5c38848a95f74c0f77
nqocmei <nqocmai201@gmail.com>
2024-06-15 18:57:48 +0700

    create component no-data

---
+++ resources/views/components/no-data.blade.php
@@ -0,0 +1,4 @@
+<div class="text-center">
+    <img width="400" height="auto" class="img-fluid" src="{{asset('svg/no-data.svg')}}" alt="Không có dữ liệu" />
+    <p class="fs-5">Bạn chưa thêm sản phẩm nào vào giỏ hàng!</p>
+</div>
