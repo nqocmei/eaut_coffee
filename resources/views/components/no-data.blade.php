cb802ffbcfb759a88af1180e5b0e0a32974dca2c
nqocmei <nqocmai201@gmail.com>
2024-06-15 19:06:38 +0700

    refactor ui

--- resources/views/components/no-data.blade.php
+++ resources/views/components/no-data.blade.php
@@ -1,4 +1,4 @@
 <div class="text-center">
-    <img width="400" height="auto" class="img-fluid" src="{{asset('svg/no-data.svg')}}" alt="Không có dữ liệu" />
-    <p class="fs-5">Bạn chưa thêm sản phẩm nào vào giỏ hàng!</p>
+    <img width="400" height="auto" class="img-fluid mb-0 pb-0" src="{{asset('svg/no-data.svg')}}" alt="Không có dữ liệu" />
+    <p class="fs-5 mt-0 pt-0">Bạn chưa thêm sản phẩm nào vào giỏ hàng!</p>
 </div>
--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,117 +1,112 @@
 @extends('layouts.site_layout')
 @section('content')
-    <style>
-        input[type="number"] {
-            -webkit-appearance: textfield;
-            -moz-appearance: textfield;
-            appearance: textfield;
-        }
+<style>
+    input[type="number"] {
+        -webkit-appearance: textfield;
+        -moz-appearance: textfield;
+        appearance: textfield;
+    }
 
-        input[type=number]::-webkit-inner-spin-button,
-        input[type=number]::-webkit-outer-spin-button {
-            -webkit-appearance: none;
-        }
-    </style>
-    <div class="body mt-5">
-        @if (Auth::check())
-            @if ($carts->count() <= 0)
-                <div class="my-5 py-5 text-center">
-                    <p class="fs-5">Bạn chưa thêm sản phẩm nào vào giỏ hàng!</p>
-                </div>
-            @else
-                <div class="table-responsive mb-2">
-                    <table id="cart" class="table table-hover table-condensed">
-                        <thead>
-                            <tr>
-                                <th class="text-center">Ảnh</th>
-                                <th class="text-center">Sản phẩm</th>
-                                <th class="text-center">Giá gốc</th>
-                                <th class="text-center">Giảm giá</th>
-                                <th class="text-center">Khuyến mại</th>
-                                <th class="text-center">Số lượng</th>
-                                <th class="text-center">Tổng tiền</th>
-                                <th class="text-center">Hành động</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php $total = 0 @endphp
-                            @foreach ($carts as $cart)
-                                @php $total += $cart->product->promotional_price * $cart->quantity @endphp
-                                <tr data-id="{{ $cart->id }}">
-                                    <td class="text-center">
-                                        <img src="{{ asset($cart->product->image_path) }}" width="100" height="100"
-                                            class="object-fit-contain" />
-                                    </td>
-                                    <td class="text-center">
-                                        <p>{{ $cart->product->name }}</p>
-                                    </td>
-                                    <td class="text-center text-decoration-line-through">
-                                        <p>{{ number_format($cart->product->price, 0, ',', '.') }}đ</p>
-                                    </td>
-                                    <td class="text-center">
-                                        <p>{{ $cart->product->discount }}%</p>
-                                    </td>
-                                    <td class="text-center">
-                                        <p>{{ number_format($cart->product->promotional_price, 0, ',', '.') }}đ</p>
-                                    </td>
-                                    <td class="text-center">
-                                        <div class="d-flex justify-content-around gap-1">
-                                            <button class="btn btn-danger decrease-btn"
-                                                data-id="{{ $cart->id }}">-</button>
-                                            <input type="number" class="form-control text-center quantity-input"
-                                                name="quantity" value="{{ $cart->quantity }}" min="1"
-                                                max="{{ $cart->product->amount }}" readonly />
-                                            <button class="btn btn-danger increase-btn"
-                                                data-id="{{ $cart->id }}">+</button>
-                                        </div>
-                                    </td>
-                                    <td class="text-center total-money">
-                                        {{ number_format($cart->product->promotional_price * $cart->quantity, 0, ',', '.') }}đ
-                                    </td>
-                                    <td class="text-center">
-                                        <button class="btn btn-danger btn-sm"
-                                            onclick="updateQuantity({{ $cart->id }}, 0)">Xóa</button>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                <h5 class="d-flex justify-content-end align-items-center">
-                    Tổng thanh toán:&nbsp;<span class="text-danger total-cart" style="font-size: 20px;">
-                        {{ number_format($total, 0, ',', '.') }}đ</span>
-                </h5>
-                <a href="{{ url('/') }}" class="btn btn-info text-white">
-                    <i class="fa fa-arrow-left"></i> Tiếp tục mua sắm
-                </a>
-                <button class="btn btn-success">
-                    <a class="text-white text-decoration-none" href="{{ route('checkout') }}">
-                        Mua hàng
-                    </a>
-                </button>
-            @endif
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+    }
+</style>
+<div class="body mt-5">
+    @if (Auth::check())
+        @if ($carts->count() <= 0)
+            @include('components.no-data')
         @else
-            <div class="my-5 py-5 text-center">
-                <p class="fs-5">Đăng nhập để sử dụng tính năng này!</p>
+            <div class="table-responsive mb-2">
+                <table id="cart" class="table table-hover table-condensed">
+                    <thead>
+                        <tr>
+                            <th class="text-center">Ảnh</th>
+                            <th class="text-center">Sản phẩm</th>
+                            <th class="text-center">Giá gốc</th>
+                            <th class="text-center">Giảm giá</th>
+                            <th class="text-center">Khuyến mại</th>
+                            <th class="text-center">Số lượng</th>
+                            <th class="text-center">Tổng tiền</th>
+                            <th class="text-center">Hành động</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php        $total = 0 @endphp
+                        @foreach ($carts as $cart)
+                            @php            $total += $cart->product->promotional_price * $cart->quantity @endphp
+                            <tr data-id="{{ $cart->id }}">
+                                <td class="text-center">
+                                    <img src="{{ asset($cart->product->image_path) }}" width="100" height="100"
+                                        class="object-fit-contain" />
+                                </td>
+                                <td class="text-center">
+                                    <p>{{ $cart->product->name }}</p>
+                                </td>
+                                <td class="text-center text-decoration-line-through">
+                                    <p>{{ number_format($cart->product->price, 0, ',', '.') }}đ</p>
+                                </td>
+                                <td class="text-center">
+                                    <p>{{ $cart->product->discount }}%</p>
+                                </td>
+                                <td class="text-center">
+                                    <p>{{ number_format($cart->product->promotional_price, 0, ',', '.') }}đ</p>
+                                </td>
+                                <td class="text-center">
+                                    <div class="d-flex justify-content-around gap-1">
+                                        <button class="btn btn-danger decrease-btn" data-id="{{ $cart->id }}">-</button>
+                                        <input type="number" class="form-control text-center quantity-input" name="quantity"
+                                            value="{{ $cart->quantity }}" min="1" max="{{ $cart->product->amount }}" readonly />
+                                        <button class="btn btn-danger increase-btn" data-id="{{ $cart->id }}">+</button>
+                                    </div>
+                                </td>
+                                <td class="text-center total-money">
+                                    {{ number_format($cart->product->promotional_price * $cart->quantity, 0, ',', '.') }}đ
+                                </td>
+                                <td class="text-center">
+                                    <button class="btn btn-danger btn-sm" onclick="updateQuantity({{ $cart->id }}, 0)">Xóa</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
+            <h5 class="d-flex justify-content-end align-items-center">
+                Tổng thanh toán:&nbsp;<span class="text-danger total-cart" style="font-size: 20px;">
+                    {{ number_format($total, 0, ',', '.') }}đ</span>
+            </h5>
+            <a href="{{ url('/') }}" class="btn btn-info text-white">
+                <i class="fa fa-arrow-left"></i> Tiếp tục mua sắm
+            </a>
+            <button class="btn btn-success">
+                <a class="text-white text-decoration-none" href="{{ route('checkout') }}">
+                    Mua hàng
+                </a>
+            </button>
         @endif
-    </div>
+    @else
+        <div class="my-5 py-5 text-center">
+            <p class="fs-5">Đăng nhập để sử dụng tính năng này!</p>
+        </div>
+    @endif
+</div>
 
-    <script type="text/javascript">
+<script type="text/javascript">
+    document.addEventListener('DOMContentLoaded', function () {
         function updateQuantity(id, quantity) {
             const csrfToken = '{{ csrf_token() }}';
 
-            fetch('{{ route('update_quantity_cart') }}', {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': csrfToken
-                    },
-                    body: JSON.stringify({
-                        id: id,
-                        quantity: quantity
-                    }),
-                })
+            fetch("{{ route('update_quantity_cart') }}", {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': csrfToken
+                },
+                body: JSON.stringify({
+                    id: id,
+                    quantity: quantity
+                }),
+            })
                 .then(response => response.json())
                 .then(data => {
                     if (data.success) {
@@ -126,11 +121,7 @@
                             row.remove();
                             document.querySelector('.total-cart').textContent = data.totalCart;
                             if (document.querySelectorAll('tbody tr').length === 0) {
-                                document.querySelector('.body').innerHTML = `
-                            <div class="my-5 py-5 text-center">
-                                <p class="fs-5">Bạn chưa thêm sản phẩm nào vào giỏ hàng!</p>
-                            </div>
-                        `;
+                                document.querySelector('.body').innerHTML = `@include('components.no-data')`;
                             }
                         }
                     } else {
@@ -145,32 +136,32 @@
                     });
                 });
         }
-        document.addEventListener('DOMContentLoaded', function() {
-            document.querySelector('tbody').addEventListener('click', function(event) {
-                const target = event.target;
-                if (target.classList.contains('increase-btn') || target.classList.contains(
-                        'decrease-btn')) {
-                    const row = target.closest('tr');
-                    const quantityInput = row.querySelector('.quantity-input');
-                    let quantity = parseInt(quantityInput.value);
 
-                    if (target.classList.contains('increase-btn')) {
-                        if (quantity < parseInt(quantityInput.max)) {
-                            quantity++;
-                            updateQuantity(row.dataset.id, quantity);
-                        }
-                    } else if (target.classList.contains('decrease-btn')) {
-                        if (quantity > 0) {
-                            quantity--;
-                            updateQuantity(row.dataset.id, quantity);
-                        }
+        document.querySelector('tbody').addEventListener('click', function (event) {
+            const target = event.target;
+            if (target.classList.contains('increase-btn') || target.classList.contains(
+                'decrease-btn')) {
+                const row = target.closest('tr');
+                const quantityInput = row.querySelector('.quantity-input');
+                let quantity = parseInt(quantityInput.value);
+
+                if (target.classList.contains('increase-btn')) {
+                    if (quantity < parseInt(quantityInput.max)) {
+                        quantity++;
+                        updateQuantity(row.dataset.id, quantity);
                     }
-
-                    quantityInput.value = quantity;
+                } else if (target.classList.contains('decrease-btn')) {
+                    if (quantity > 0) {
+                        quantity--;
+                        updateQuantity(row.dataset.id, quantity);
+                    }
                 }
-            });
 
-
+                quantityInput.value = quantity;
+            }
         });
-    </script>
+
+
+    });
+</script>
 @endsection
